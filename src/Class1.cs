7fc06ae08a03173d05e5284ac66522ace1b2e295
cwongmath <cwongmath@gmail.com>
2019-07-10 17:34:06 -0500

    Configure Kopernicus' nuget

--- src/Class1.cs
+++ src/Class1.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Kopernicus;
 using Kopernicus.ConfigParser;
+using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using Kopernicus.ConfigParser.Enumerations;
+using Kopernicus.ConfigParser.Interfaces;
 using Kopernicus.Configuration.ModLoader;
+using Kopernicus.Configuration.Parsing;
 using Kopernicus.Configuration;
+using Kopernicus.UI;
 
 namespace KopernicusPQSExtensions
 {
