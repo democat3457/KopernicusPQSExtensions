923191ca9bab02a6653300cc737a91370651bf5a
cwongmath <cwongmath@gmail.com>
2019-07-10 16:49:13 -0500

    Basic VS Stuff

---
+++ src/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kopernicus;
+using Kopernicus.ConfigParser;
+using Kopernicus.Configuration.ModLoader;
+using Kopernicus.Configuration;
+
+namespace KopernicusPQSExtensions
+{
+    public class Class1
+    {
+    }
+}
