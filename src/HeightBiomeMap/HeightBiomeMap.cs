be289e01f9439559e184306d825a1da7f64fc0e9
democat3457 <cwongmath@gmail.com>
2019-07-10 20:55:21 -0500

    Move into HeightBiomeMap folder

---
+++ src/HeightBiomeMap/HeightBiomeMap.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Kopernicus;
+using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using Kopernicus.ConfigParser.Enumerations;
+using Kopernicus.ConfigParser.Interfaces;
+using Kopernicus.Configuration.ModLoader;
+using Kopernicus.Configuration.Parsing;
+using Kopernicus.UI;
+
+namespace KopernicusPQSExtensions
+{
+    public class HeightBiomeMap : ModLoader<PQSMod_HeightBiomeMap>
+    {
+        [RequireConfigType(ConfigType.Node)]
+        public class LandClassLoader : IPatchable, ITypeParser<PQSMod_HeightBiomeMap.LandClass>
+        {
+            // Land class object
+            public PQSMod_HeightBiomeMap.LandClass Value { get; set; }
+
+            // Name of the class
+            [ParserTarget("name")]
+            public String name
+            {
+                get { return Value.name; }
+                set { Value.name = value; }
+            }
+
+            // Delete the landclass
+            [ParserTarget("delete")]
+            [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+            public NumericParser<Boolean> Delete = false;
+
+            // Color of the class
+            [ParserTarget("color")]
+            public ColorParser Color
+            {
+                get { return Value.color; }
+                set { Value.color = value; }
+            }
+
+            // Fractional altitude start
+            // altitude = (vertexHeight - vertexMinHeightOfPQS) / vertexHeightDeltaOfPQS
+            [ParserTarget("altitudeStart")]
+            public NumericParser<Double> AltitudeStart
+            {
+                get { return Value.altStart; }
+                set { Value.altStart = value; }
+            }
+
+            // Fractional altitude end
+            [ParserTarget("altitudeEnd")]
+            public NumericParser<Double> AltitudeEnd
+            {
+                get { return Value.altEnd; }
+                set { Value.altEnd = value; }
+            }
+
+            // Should we blend into the next class
+            [ParserTarget("lerpToNext")]
+            public NumericParser<Boolean> LerpToNext
+            {
+                get { return Value.lerpToNext; }
+                set { Value.lerpToNext = value; }
+            }
+
+            [KittopiaConstructor(KittopiaConstructor.ParameterType.Empty)]
+            public LandClassLoader()
+            {
+                // Initialize the land class
+                Value = new PQSMod_HeightBiomeMap.LandClass("class", 0.0, 0.0, UnityEngine.Color.white, UnityEngine.Color.white, 0.0);
+            }
+
+            public LandClassLoader(PQSMod_HeightBiomeMap.LandClass c)
+            {
+                Value = c;
+            }
+
+            /// <summary>
+            /// Convert Parser to Value
+            /// </summary>
+            public static implicit operator PQSMod_HeightBiomeMap.LandClass(LandClassLoader parser)
+            {
+                return parser.Value;
+            }
+
+            /// <summary>
+            /// Convert Value to Parser
+            /// </summary>
+            public static implicit operator LandClassLoader(PQSMod_HeightBiomeMap.LandClass value)
+            {
+                return new LandClassLoader(value);
+            }
+        }
+        
+        [ParserTarget("blend")]
+        public NumericParser<Single> Blend
+        {
+            get { return Mod.blend; }
+            set { Mod.blend = value.Value; }
+        }
+
+        [ParserTargetCollection("LandClasses", AllowMerge = true)]
+        public CallbackList<LandClassLoader> LandClasses { get; set; }
+
+        // Creates the a PQSMod of type T with given PQS
+        public override void Create(PQS pqsVersion)
+        {
+            base.Create(pqsVersion);
+
+            // Create the callback list
+            LandClasses = new CallbackList<LandClassLoader>(e =>
+            {
+                Mod.landClasses = LandClasses.Where(landClass => !landClass.Delete)
+                    .Select(landClass => landClass.Value).ToArray();
+                Mod.lcCount = Mod.landClasses.Length;
+            });
+            Mod.landClasses = new PQSMod_HeightBiomeMap.LandClass[Mod.lcCount = 0];
+        }
+
+        // Grabs a PQSMod of type T from a parameter with a given PQS
+        public override void Create(PQSMod_HeightBiomeMap mod, PQS pqsVersion)
+        {
+            base.Create(mod, pqsVersion);
+
+            // Create the callback list
+            LandClasses = new CallbackList<LandClassLoader>(e =>
+            {
+                Mod.landClasses = LandClasses.Where(landClass => !landClass.Delete)
+                    .Select(landClass => landClass.Value).ToArray();
+                Mod.lcCount = Mod.landClasses.Length;
+            });
+
+            // Load LandClasses
+            if (Mod.landClasses != null)
+            {
+                for (Int32 i = 0; i < Mod.landClasses.Length; i++)
+                {
+                    // Only activate the callback if we are adding the last loader
+                    LandClasses.Add(new LandClassLoader(Mod.landClasses[i]), i == Mod.landClasses.Length - 1);
+                }
+            }
+            else
+            {
+                Mod.landClasses = new PQSMod_HeightBiomeMap.LandClass[Mod.lcCount = 0];
+            }
+        }
+    }
+}
---
+++ src/HeightBiomeMap/PQSMod_HeightBiomeMap.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Kopernicus;
+using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using Kopernicus.ConfigParser.Enumerations;
+using Kopernicus.ConfigParser.Interfaces;
+using Kopernicus.Configuration.ModLoader;
+using Kopernicus.Configuration.Parsing;
+using Kopernicus.UI;
+using UnityEngine;
+
+namespace KopernicusPQSExtensions
+{
+    public class PQSMod_HeightBiomeMap : PQSMod
+    {
+        public PQSMod_HeightBiomeMap()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Token: 0x06005313 RID: 21267 RVA: 0x00029EA8 File Offset: 0x000280A8
+        public override void OnSetup()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Token: 0x06005314 RID: 21268 RVA: 0x00029EB0 File Offset: 0x000280B0
+        public override void OnVertexBuild(PQS.VertexBuildData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Token: 0x06005315 RID: 21269 RVA: 0x00029EB8 File Offset: 0x000280B8
+        public void SelectLandClassByHeight(double height)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Token: 0x06005316 RID: 21270 RVA: 0x00029EC0 File Offset: 0x000280C0
+        public static double Lerp(double v2, double v1, double dt)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Token: 0x06005317 RID: 21271 RVA: 0x00029EC8 File Offset: 0x000280C8
+        public double CubicHermite(double start, double end, double startTangent, double endTangent, double t)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Token: 0x06005318 RID: 21272 RVA: 0x00029ED0 File Offset: 0x000280D0
+        public static double Clamp(double v, double low, double high)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Token: 0x0400453B RID: 17723
+        public PQSMod_HeightColorMap.LandClass[] landClasses;
+
+        // Token: 0x0400453C RID: 17724
+        public float blend;
+
+        // Token: 0x0400453D RID: 17725
+        [HideInInspector]
+        public int lcCount;
+
+        // Token: 0x02000CC2 RID: 3266
+        [Serializable]
+        public class LandClass
+        {
+            // Token: 0x06005319 RID: 21273 RVA: 0x00029ED8 File Offset: 0x000280D8
+            public LandClass(string name, double fractalStart, double fractalEnd, Color baseColor, Color colorNoise, double colorNoiseAmount)
+            {
+                throw new NotImplementedException();
+            }
+
+            // Token: 0x17000BB8 RID: 3000
+            // (get) Token: 0x0600531A RID: 21274 RVA: 0x00029EE0 File Offset: 0x000280E0
+            // (set) Token: 0x0600531B RID: 21275 RVA: 0x00029EE8 File Offset: 0x000280E8
+            public double FractalDelta
+            {
+                [CompilerGenerated]
+                get
+                {
+                    throw new NotImplementedException();
+                }
+                [CompilerGenerated]
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            // Token: 0x0400453E RID: 17726
+            public string name;
+
+            // Token: 0x0400453F RID: 17727
+            public double altStart;
+
+            // Token: 0x04004540 RID: 17728
+            public double altEnd;
+
+            // Token: 0x04004541 RID: 17729
+            public Color color;
+
+            // Token: 0x04004542 RID: 17730
+            public bool lerpToNext;
+        }
+    }
+}
