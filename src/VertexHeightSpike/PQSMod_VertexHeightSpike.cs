dbdf2032d28fdd75dd6c86dcbb6f5c7f8ad74af1
democat3457 <cwongmath@gmail.com>
2019-07-12 11:51:51 -0500

    Add sample pqsmod

---
+++ src/VertexHeightSpike/PQSMod_VertexHeightSpike.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace KopernicusPQSExtensions.VertexHeightSpike
+{
+    public class PQSMod_VertexHeightSpike : PQSMod
+    {
+        public SpikeLevel spikeLevel;
+
+        public Boolean doubleSpikeLevel;
+
+        public Double spikeHeight;
+
+        public override void OnSetup()
+        {
+            spikeHeight = ((double) spikeLevel) / 10;
+            if (doubleSpikeLevel)
+            {
+                spikeHeight *= 2;
+            }
+        }
+        
+        public override void OnVertexBuildHeight(PQS.VertexBuildData data)
+        {
+            data.vertHeight *= spikeHeight;
+        }
+
+        public enum SpikeLevel
+        {
+            Low = 20,
+            Standard = 30,
+            Medium = 35,
+            High = 50
+        }
+    }
+}
---
+++ src/VertexHeightSpike/VertexHeightSpike.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using Kopernicus.Configuration.ModLoader;
+
+namespace KopernicusPQSExtensions.VertexHeightSpike
+{
+    public class VertexHeightSpike : ModLoader<PQSMod_VertexHeightSpike>
+    {
+        [ParserTarget("spikeLevel")]
+        public EnumParser<PQSMod_VertexHeightSpike.SpikeLevel> SpikeLevel
+        {
+            get { return base.Mod.spikeLevel; }
+            set { base.Mod.spikeLevel = value.Value; }
+        }
+
+        [ParserTarget("doubleSpikeLevel")]
+        public NumericParser<Boolean> DoubleSpikeLevel
+        {
+            get { return Mod.doubleSpikeLevel; }
+            set { Mod.doubleSpikeLevel = value; }
+        }
+    }
+}
